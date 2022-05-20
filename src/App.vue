19675d9a92a61ac959e0382f6e3098c951a8d8d2
lai <a2323178080@gmail.com>
2022-05-20 21:21:57 +0800

    0520

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Alert />
+  <GoTop/>
+  <RouterView />
+</template>
+
+<script>
+import Alert from '@/components/AlertMessage.vue'
+import GoTop from '@/components/GoTop.vue'
+export default {
+  name: 'APP',
+  components: {
+    Alert,
+    GoTop
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/scss/all.scss';
+</style>
+
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicPath: '/dist/',
+  lintOnSave: false,
+}
+
+
